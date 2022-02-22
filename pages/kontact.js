4d9fde52d14b7457a42bf2cabe0d3c704cf30ca7
Thiago Messias <thiagomessiasdevbr@gmail.com>
2022-02-22 11:47:51 +0000

    pages

---
+++ pages/kontact.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Kontact extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Loja</h1>
+            </div>
+        )
+    }
+}
+
+export default Kontact;
---
+++ pages/negocios.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Negocio extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Loja</h1>
+            </div>
+        )
+    }
+}
+
+export default Negocio;
